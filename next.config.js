db6f1aec77e46f401dbe9849245107bc567a7d86
mizchi <miz404@gmail.com>
2018-03-15 17:33:33 +0900

    It works

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
+const routes = require('./routes')
+
 module.exports = {
   async exportPathMap() {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' }
-    }
+    return routes
   },
   webpack(config, options) {
     if (!options.defaultLoaders) {
@@ -11,16 +10,6 @@ module.exports = {
         'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
       )
     }
-
-    if (options.isServer) {
-      config.externals = ['react', 'react-dom', ...config.externals]
-    }
-
-    config.resolve.alias = Object.assign({}, config.resolve.alias, {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat'
-    })
-
     return config
   }
 }
---
+++ routes.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '/': { page: '/' },
+  '/about': { page: '/about' }
+}
