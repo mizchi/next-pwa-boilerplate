b84a10fb3160dbec96cccffcff1fbbd9cab8aacf
mizchi <miz404@gmail.com>
2018-03-16 01:03:02 +0900

    Fix lints

--- src/client/components/molecules/Header.js
+++ src/client/components/molecules/Header.js
@@ -1,4 +1,5 @@
 /* @flow */
+import React from 'react'
 import Link from 'next/link'
 
 export default function Header() {
--- src/client/pages/_document.js
+++ src/client/pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({ renderPage }: any) {
     const sheet = new ServerStyleSheet()
     const page = renderPage(App => props =>
       sheet.collectStyles(<App {...props} />)
